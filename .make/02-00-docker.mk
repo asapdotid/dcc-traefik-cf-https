3d763e3c30aa3f9570c2829bb58bd6407d78d67f
Asapdotid <asapdotid@gmail.com>
2023-09-25 19:35:01 +0700

    👌 IMPROVE: update makefile docker scripts

--- .make/02-00-docker.mk
+++ .make/02-00-docker.mk
@@ -1,7 +1,6 @@
 # For local builds we always want to use "latest" as tag per default
-ifeq ($(ENV),local)
-	TAG:=latest
-endif
+TAG:=latest
+
 
 # Enable buildkit for docker and docker-compose by default for every environment.
 # For specific environments (e.g. MacBook with Apple Silicon M1 CPU) it should be turned off to work stable
@@ -20,25 +19,22 @@ DOCKER_SERVICE_NAME_LOGGER:=logger
 
 # FYI:
 # Naming convention for images is $(DOCKER_REGISTRY)/$(DOCKER_NAMESPACE)/$(DOCKER_SERVICE_NAME)-$(ENV)
-# e.g.               docker.io/asapdotid/traefik-local
+# e.g.               docker.io/asapdotid/traefik:latest
 # $(DOCKER_REGISTRY)---^          ^       ^      ^        docker.io
 # $(DOCKER_NAMESPACE)-------------^       ^      ^        asapdotid
 # $(DOCKER_SERVICE_NAME)------------------^      ^        traefik
-# $(ENV)-----------------------------------------^        local
 
-DOCKER_DIR:=${PWD}/src
+DOCKER_DIR:=$(CURDIR)/src
 DOCKER_ENV_FILE:=$(DOCKER_DIR)/.env
 DOCKER_COMPOSE_DIR:=$(DOCKER_DIR)/compose
 DOCKER_COMPOSE_FILE:=$(DOCKER_COMPOSE_DIR)/compose.yml
-DOCKER_COMPOSE_FILE_ENV:=$(DOCKER_COMPOSE_DIR)/compose.$(ENV).yml
+DOCKER_COMPOSE_FILE_ENV:=$(DOCKER_COMPOSE_DIR)/compose.local.yml
 
 # we need a couple of environment variables for docker-compose so we define a make-variable that we can
 # then reference later in the Makefile without having to repeat all the environment variables
 DOCKER_COMPOSE_COMMAND:= \
-    ENV=$(ENV) \
     DOCKER_REGISTRY=$(DOCKER_REGISTRY) \
     DOCKER_NAMESPACE=$(DOCKER_NAMESPACE) \
-    TIMEZONE=$(TIMEZONE) \
     TAG=$(TAG) \
     docker compose -p $(DOCKER_PROJECT_NAME) --env-file $(DOCKER_ENV_FILE)
 
@@ -71,22 +67,21 @@ endif
 
 ##@ [Docker]
 
-.PHONY: docker-init
-docker-init: .docker/.env ## Docker initial environment
-docker-init:
+.PHONY: set-init
+set-init: .docker/.env ## Docker initial environment
+set-init:
 	@echo "Please update your src/.env file with your settings"
 
-.PHONY: docker-clean-env
-docker-clean-env: ## Remove the .env file for docker
+.PHONY: rm-env
+rm-env: ## Remove the .env file for docker
 	@rm -f $(DOCKER_ENV_FILE)
 
 .PHONY: validate-docker-variables
 validate-docker-variables:
 	@$(if $(TAG),,$(error TAG is undefined))
-	@$(if $(ENV),,$(error ENV is undefined))
 	@$(if $(DOCKER_REGISTRY),,$(error DOCKER_REGISTRY is undefined - Did you run make-init?))
 	@$(if $(DOCKER_NAMESPACE),,$(error DOCKER_NAMESPACE is undefined - Did you run make-init?))
-	@$(if $(TIMEZONE),,$(error TIMEZONE is undefined - Did you run make-init?))
+	@$(if $(DOCKER_PROJECT_NAME),,$(error DOCKER_PROJECT_NAME is undefined - Did you run make-init?))
 
 .docker/.env:
 	@cp $(DOCKER_ENV_FILE).example $(DOCKER_ENV_FILE)
@@ -95,39 +90,39 @@ validate-docker-variables:
 docker-build-image: validate-docker-variables ## Build all docker images OR a specific image by providing the service name via: make docker-build DOCKER_SERVICE_NAME=<service>
 	@$(DOCKER_COMPOSE) build $(DOCKER_SERVICE_NAME)
 
-.PHONY: docker-build
-docker-build: docker-build-image ## Build the php image and then all other docker images
+.PHONY: build
+build: docker-build-image ## Build the php image and then all other docker images
 
-.PHONY: docker-clean
-docker-clean: ## Removing dangling and unused images
+.PHONY: clean
+clean: ## Removing dangling and unused images
 	@docker rmi -f $$(docker images -f "dangling=true" -q)
 
-.PHONY: docker-prune
-docker-prune: ## Remove ALL unused docker resources, including volumes
+.PHONY: prune
+prune: ## Remove ALL unused docker resources, including volumes
 	@docker system prune -a -f --volumes
 
 ##@ [Docker: Compose]
 
-.PHONY: compose-up
-compose-up: validate-docker-variables ## Create and start all docker containers. To create/start only a specific container, use DOCKER_SERVICE_NAME=<service>
+.PHONY: up
+up: validate-docker-variables ## Create and start all docker containers. To create/start only a specific container, use DOCKER_SERVICE_NAME=<service>
 	@$(DOCKER_COMPOSE) up -d $(DOCKER_SERVICE_NAME)
 
-.PHONY: compose-down
-compose-down: validate-docker-variables ## Stop and remove all docker containers.
+.PHONY: down
+down: validate-docker-variables ## Stop and remove all docker containers.
 	@$(DOCKER_COMPOSE) down --remove-orphans -v
 
-.PHONY: compose-restart
-compose-restart: validate-docker-variables ## Restart docker containers.
+.PHONY: restart
+restart: validate-docker-variables ## Restart docker containers.
 	@$(DOCKER_COMPOSE) restart $(DOCKER_SERVICE_NAME)
 
-.PHONY: compose-config
-compose-config: validate-docker-variables ## List the configuration
+.PHONY: config
+config: validate-docker-variables ## List the configuration
 	@$(DOCKER_COMPOSE) config $(DOCKER_SERVICE_NAME)
 
-.PHONY: compose-logs
-compose-logs: validate-docker-variables ## Logs docker containers.
+.PHONY: logs
+logs: validate-docker-variables ## Logs docker containers.
 	@$(DOCKER_COMPOSE) logs --tail=100 -f $(DOCKER_SERVICE_NAME)
 
-.PHONY: compose-ps
-compose-ps: validate-docker-variables ## Docker composer PS containers.
+.PHONY: ps
+ps: validate-docker-variables ## Docker composer PS containers.
 	@$(DOCKER_COMPOSE) ps $(DOCKER_SERVICE_NAME)
